e355c76d5c9dc3c87b47a0c0e0c9cb7a18b120b9
Tim <tim.martensson92@gmail.com>
2024-04-03 10:08:55 +0200

    api done

--- WebApi/Controllers/ContactController.cs
+++ WebApi/Controllers/ContactController.cs
@@ -1,11 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Dtos;
+using WebApi.Filters;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ContactController : ControllerBase
+    [UseApiKey]
+    public class ContactController(ContactService contactService) : ControllerBase
     {
+        private readonly ContactService _contactService = contactService;
+
+        [HttpPost]
+        public async Task<IActionResult> SendContactFormAsync(ContactFormDto dto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var contactForm = await _contactService.CreateContactFormAsync(dto);
+                    if (contactForm != null)
+                    {
+                        return Created();
+                    }
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
--- WebApi/Services/ContactService.cs
+++ WebApi/Services/ContactService.cs
@@ -1,6 +1,32 @@
-﻿namespace WebApi.Services
+﻿using WebApi.Dtos;
+using WebApi.Entities;
+using WebApi.Repositories;
+
+namespace WebApi.Services;
+
+public class ContactService(ContactRepository contactRepository)
 {
-    public class ContactService
+    private readonly ContactRepository _contactRepository = contactRepository;
+
+    public async Task<ContactEntity> CreateContactFormAsync(ContactFormDto dto)
     {
+        try
+        {
+
+            var contactForm = new ContactEntity
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                SelectedOption = dto.SelectedOption,
+                Message = dto.Message
+            };
+
+            return await _contactRepository.CreateOne(contactForm);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
+
 }
